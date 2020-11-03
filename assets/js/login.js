38ff640c5975a20c317f14293625f8aeb64d6851
zhangxinwei <1121462572@qq.com>
2020-11-03 10:57:19 +0800

    完成了注册喝登录页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -22,40 +22,37 @@ $(function() {
             }
         }
     })
-    var data = {
-        username: $('#form_reg [name=username]').val(),
-        password: $('#form_reg [name=password]').val()
-    }
+
     $('#form_reg').on('submit', function(e) {
         e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
         $.post('/api/reguser', data, function(res) {
-            if (res.status != 0) {
-                layer.msg(res.message)
+            if (res.status !== 0) {
+                return layer.msg(res.message)
             }
-            layer.msg('注册成功了')
+            layer.msg(res.message)
             $('#link_login').click()
-
         })
-
     })
 
     $('#form_login').submit(function(e) {
         e.preventDefault()
         $.ajax({
-            type: "POST",
-            url: "/api/login",
-            //快速获取表单数据
+            method: 'POST',
+            url: '/api/login',
             data: $(this).serialize(),
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg('登录失败！')
+                    return layer.msg(res.message)
                 }
-                layer.msg('登录成功')
-                    // console.log(res.token);
-                    //跳转后台主页
+                layer.msg(res.message)
                 localStorage.setItem('token', res.token)
-                location.href = '/大事件/index.html'
+                location.href = '/index.html'
             }
-        });
+        })
     })
+
 })
